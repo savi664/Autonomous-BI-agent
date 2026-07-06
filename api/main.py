db95a50f3572cbdc9e801050fbd8c168c16a5fb8
savi664 <savinugunarathna4@gmail.com>
2026-07-07 04:04:42 +0530

    Lock CORS to Vercel URL

--- api/main.py
+++ api/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://autonomous-bi-agent.vercel.app", "http://localhost:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
