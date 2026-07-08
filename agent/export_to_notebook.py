a1efcf2311b902895cdc3962eb9a236f6325143a
savi664 <savinugunarathna4@gmail.com>
2026-07-08 10:24:54 +0530

    Remove embedded CSV from notebook export, use placeholder instead

--- agent/export_to_notebook.py
+++ agent/export_to_notebook.py
@@ -1,19 +1,16 @@
 import nbformat as nbf
 
 
-def export_to_notebook(hypotheses: list, csv_text: str):
-    escaped = csv_text.replace("\\", "\\\\").replace('"""', '\\"\\"\\"')
-    setup_code = f'''
-import pandas as pd
-from io import StringIO
-csv_data = """{escaped}"""
-df = pd.read_csv(StringIO(csv_data))
-'''
-    # Create a note book object
+def export_to_notebook(hypotheses: list, csv_text: str = ""):
     nb = nbf.v4.new_notebook()
     cells = []
     cells.append(nbf.v4.new_markdown_cell("InsightFlow Analysis Report"))
-    cells.append(nbf.v4.new_code_cell(setup_code))
+    cells.append(
+        nbf.v4.new_code_cell(
+            "# TODO: Set your CSV file path here\n"
+            'df = pd.read_csv("path/to/your/file.csv")'
+        )
+    )
     for hypothesis in hypotheses:
         cells.append(
             nbf.v4.new_markdown_cell(f"## Hypothesis: {hypothesis['question']}\n")
